e22685e08e9b1d0d91899197ca5ec5f67172e514
nguyen ha trung <hatrungngn@gmail.com>
2025-12-17 17:47:36 +0700

    update Frontend

--- Backend/routes/playerRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require("koa-router");
-const playerController = require("../controller/playerController");
-const router = Router();
-const { authorize } = require("../middleware");
-
-router.get("/player/:username", authorize, playerController.getPlayer);
-router.get("/player/rankings", authorize, playerController.getRankPlayer);
-
-module.exports = router;
